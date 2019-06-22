7c3dd30c6b5399d6f99df226a438bf7d1cd36add
Kaja <30932310+KajaMaria@users.noreply.github.com>
2019-06-22 15:57:33 +0100

    Adding step 9

--- directory.rb
+++ directory.rb
@@ -1,52 +1,60 @@
-#let's put all students into an array
-
-
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name
+  # get the first names
+  puts "What is your name"
   name = gets.chomp
+  puts "What is your cohort"
+  cohort = gets.chomp || :november
+
+
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+  while !name.empty? && !cohort.empty? do
+    # add the student to the hash array
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "What is your name?"
     name = gets.chomp
+    puts "What is your cohort?"
+    cohort = gets.chomp
+      # if name.empty? || cofort.empty?
+      #   puts empty = gets.chomp.to_sym
+      # end
+
   end
   # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------"
+puts "The students of Villains"
+puts "-------------"
 end
+
 def print(students)
+  students_by_cohort = {}
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    if students_by_cohort[student[:cohort]] == nil
+      students_by_cohort[student[:cohort]] = []
+    end
+    students_by_cohort[student[:cohort]].push(student[:name])
+
+    # puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
+  puts students_by_cohort.flatten
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count > 1
+    puts "Overall, we have #{names.count} great students"
+  elsif names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  end
 end
-#nothing happens until we call the methods
+
 students = input_students
 print_header
 print(students)
